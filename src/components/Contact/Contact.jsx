ac48e69f48797ffe80d1fd04d269822c45e01c32
Awais Ansari <awais.ansari.6012@gmail.com>
2024-07-06 16:49:30 +0500

    Added image in contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,12 +2,14 @@ import React, { useContext, useRef, useState } from "react";
 import "./Contact.css";
 import emailjs from "@emailjs/browser";
 import { themeContext } from "../../Context";
-import { hover } from "@testing-library/user-event/dist/hover";
+import contactImage from "../../img/contact-image.png"; // Import the image
+
 const Contact = () => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
   const form = useRef();
-  const [done, setDone] = useState(false)
+  const [done, setDone] = useState(false);
+
   const sendEmail = (e) => {
     e.preventDefault();
 
@@ -22,7 +24,7 @@ const Contact = () => {
         (result) => {
           console.log(result.text);
           setDone(true);
-          form.reset();
+          form.current.reset(); // Fix reset issue
         },
         (error) => {
           console.log(error.text);
@@ -32,30 +34,25 @@ const Contact = () => {
 
   return (
     <div className="contact-form" id="contact">
-      {/* left side copy and paste from work section */}
+      {/* Left side */} 
       <div className="w-left">
         <div className="awesome">
           {/* darkMode */}
-          <span style={{color: darkMode?'white': ''}}>Get in Touch</span>
+          <span style={{ color: darkMode ? 'white' : '' }}>Get in Touch</span>
           <span>Contact me</span>
-          <div
-            className="blur s-blur1"
-            style={{ background: "#ABF1FF94" }}
-          ></div>
+          <img src={contactImage} alt="Contact" className="contact-image" /> {/* Added image */}
+          <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
         </div>
       </div>
-      {/* right side form */}
+      {/* Right side form */}
       <div className="c-right">
         <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name="user_name" className="user"  placeholder="Name"/>
-          <input type="email" name="user_email" className="user" placeholder="Email"/>
-          <textarea name="message" className="user" placeholder="Message"/>
-          <a href="https://wa.me/923035897703" target="_blank"><input value="Send" className="button"/></a>
-          <span>{done && "Thanks for Contacting me"}</span>
-          <div
-            className="blur c-blur1"
-            style={{ background: "var(--purple)" }}
-          ></div>
+          <input type="text" name="user_name" className="user" placeholder="Name" />
+          <input type="email" name="user_email" className="user" placeholder="Email" />
+          <textarea name="message" className="user" placeholder="Message" />
+          <input type="submit" value="Send" className="button" />
+          <span>{done && "Thanks for contacting me!"}</span>
+          <div className="blur c-blur1" style={{ background: "var(--purple)" }}></div>
         </form>
       </div>
     </div>
